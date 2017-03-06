bc3888157b440262baf52415a453470e9bc6f3ea
lmc2179 <louiscialdella@gmail.com>
2017-03-05 23:25:11 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='nonpyrametric',
+      version='0.1',
+      description='nonparametric statistics library',
+      author='Louis Cialdella',
+      author_email='louiscialdella@gmail.com',
+      url='https://github.com/lmc2179/nonpyrametric',
+      packages=['nonpyrametric'],
+     )
