6063d670ab39ddaf7f0db02ed0db1b516821604d
lmc2179 <louiscialdella@gmail.com>
2017-03-05 23:00:34 -0800

    added support for cadlag step functions

---
+++ nonpyrametric/cadlag.py
@@ -0,0 +1,31 @@
+class CadlagStepFunction(object):
+    def __init__(self, X, Y, invertible=True):
+        self.lookup = {x: y for x, y in zip(X, Y)}
+        self.X, self.Y = zip(*sorted(zip(X, Y)))
+        if invertible:
+            self.inverse = CadlagStepFunction(Y, X, invertible=False)
+
+    def __call__(self, x):
+        if x in self.lookup:
+            return self.lookup[x]
+        else:
+            return self._find_nearest_bound(x, self.X, 0, len(self.X)-1)
+
+    def _find_nearest_bound(self, x, A, l, r):
+        i = int((l+r)/2)
+        print(l, i, r, x, A[i])
+        if x == A[i]:
+            return A[i]
+        if x > A[i]:
+            if i == len(A)-1:
+                return A[-1]
+            else:
+                if x < A[i+1]:
+                    return i
+                else:
+                    return self._find_nearest_bound(x, A, i+1, r)
+        elif x < A[i]:
+            if i == 0:
+                return A[0]
+            else:
+                return self._find_nearest_bound(x, A, l, i)
---
+++ nonpyrametric/tests/test_cadlag.py
@@ -0,0 +1,31 @@
+import unittest
+from nonpyrametric import cadlag
+
+class CadlagStepFunctionTest(unittest.TestCase):
+    def test_recall(self):
+        X = [0,1,4]
+        Y = [0, 10, 40]
+        f = cadlag.CadlagStepFunction(X=X, Y=Y)
+        for x, y in zip(X, Y):
+            self.assertEqual(f(x), y)
+
+    def test_interpolation(self):
+        X = [0, 1, 2]
+        Y = [0, 1, 2]
+        f = cadlag.CadlagStepFunction(X, Y)
+        self.assertEqual(f(0.5), 0)
+        self.assertEqual(f(0.01), 0)
+        self.assertEqual(f(0.99), 0)
+        self.assertEqual(f(1.5), 1)
+        self.assertEqual(f(1.01), 1)
+        self.assertEqual(f(1.99), 1)
+        self.assertEqual(f(2.5), 2)
+        self.assertEqual(f(2.01), 2)
+        self.assertEqual(f(2.99), 2)
+
+    def test_bounds(self):
+        X = [0, 1, 2]
+        Y = [0, 1, 2]
+        f = cadlag.CadlagStepFunction(X, Y)
+        self.assertEqual(f(5), 2)
+        self.assertEqual(f(-1), 0)
