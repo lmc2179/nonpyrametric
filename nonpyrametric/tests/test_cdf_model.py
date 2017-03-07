743a83c044ccdfb121ea9edf2cbaa94b82e11e5f
lmc2179 <louiscialdella@gmail.com>
2017-03-06 23:50:15 -0800

    added unit test for CI

--- nonpyrametric/tests/test_cdf_model.py
+++ nonpyrametric/tests/test_cdf_model.py
@@ -1,5 +1,7 @@
 import unittest
 from nonpyrametric.cdf_model import CDFModel
+import numpy as np
+from scipy.stats import norm
 
 class CDFModelTest(unittest.TestCase):
     def test_bernoulli_data(self):
@@ -9,3 +11,16 @@ class CDFModelTest(unittest.TestCase):
         self.assertEqual(m.cdf(0), 0.5)
         self.assertEqual(m.cdf(0.5), 0.5)
         self.assertEqual(m.cdf(1), 1.0)
+
+    def test_ci_coverage_normal(self):
+        normal_rvs = np.random.normal(0, 1, 50)
+        m = CDFModel()
+        m.fit(normal_rvs)
+        X = np.linspace(-2, 2)
+        for x in X:
+            true_val = norm.cdf(x, 0, 1)
+            low, high = m.cdf_ci(x, alpha=0.01)
+            self.assertLess(low, true_val)
+            self.assertGreaterEqual(low, 0)
+            self.assertGreater(high, true_val)
+            self.assertLessEqual(high, 1)
